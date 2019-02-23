feee54faf7c86a4a8679af686bc86d4808378d80
Alessio Moretti <alessio.moretti@live.it>
2019-02-23 20:39:39 +0100

    Add argv correct invocation

--- main.py
+++ main.py
@@ -2,9 +2,14 @@ from utils.reader import read_jobs
 from scheduling.algorithm import *
 from scheduling.bnb.bnb import scheduling_1rC_BnB
 
+from sys import argv
+
 if __name__ == "__main__":
+    if len(argv) != 2:
+        print("Usage error: <input_sample_csv>")
+
     # reading jobs 
-    jobs = read_jobs("./samples/test_sample_2.csv")
+    jobs = read_jobs(argv[1])
     for job in jobs:
         print("JOB {0} -> starting {1} -> processing time required {2}".format(job.id, job.release_date, job.processing_time))
     print("TOTAL JOBS: {0}\n\n".format(len(jobs)))
